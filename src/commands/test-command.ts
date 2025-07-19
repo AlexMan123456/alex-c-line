089f06434de1e101f3b6fad0c65ecd94e7419171
AlexMan123456 <alex.man03888@gmail.com>
2025-07-19 19:13:07 +0100

    Initial commit

---
+++ src/commands/test-command.ts
@@ -0,0 +1,9 @@
+import { Command } from 'commander';
+export function loadCommands(program: Command) {
+program
+.command('test-command')
+.description('Quick test command')
+.action(() => {
+console.log('Success!');
+});
+}
