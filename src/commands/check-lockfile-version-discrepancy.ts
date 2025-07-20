841d88be41b7335bf6f8a20bedd943b1bda9c4c9
AlexMan123456 <alex.man03888@gmail.com>
2025-07-20 18:56:38 +0100

    Add command to check for discrepancies between version number in package.json and package-lock.json

---
+++ src/commands/check-lockfile-version-discrepancy.ts
@@ -0,0 +1,29 @@
+import { Command } from "commander";
+import { readFileSync } from "fs";
+import path from "path";
+
+function checkLockfileVersionDiscrepancy(program: Command) {
+  program
+    .command("check-lockfile-version-discrepancy")
+    .description(
+      "Check that version numbers in package.json and package-lock.json match",
+    )
+    .action(() => {
+      console.log(
+        "Checking for package.json and package-lock.json discrepancies...",
+      );
+      const { version: packageVersion } = JSON.parse(
+        readFileSync(path.resolve(process.cwd(), "package.json"), "utf-8"),
+      );
+      const { version: packageLockVersion } = JSON.parse(
+        readFileSync(path.resolve(process.cwd(), "package-lock.json"), "utf-8"),
+      );
+      if (packageVersion !== packageLockVersion) {
+        // deal with this later
+        return;
+      }
+      console.log("package.json and package-lock.json versions in sync.");
+    });
+}
+
+export default checkLockfileVersionDiscrepancy;
---
+++ tests/check-lockfile-version-discrepancy.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from "vitest";
+import { temporaryDirectoryTask } from "tempy";
+import path from "path";
+import { writeFile } from "fs/promises";
+import { execa } from "execa";
+
+describe("check-lockfile-version-discrepancy", () => {
+  test("Succeed if version numbers in package.json and package-lock.json are successful", async () => {
+    await temporaryDirectoryTask(async (tempDirectory) => {
+      const packagePath = path.join(tempDirectory, "package.json");
+      const packageLockPath = path.join(tempDirectory, "package-lock.json");
+      const localDistDirectory = path.resolve(process.cwd(), "dist");
+
+      await writeFile(
+        packagePath,
+        JSON.stringify({ version: "1.0.0" }, null, 2),
+      );
+      await writeFile(
+        packageLockPath,
+        JSON.stringify({ version: "1.0.0" }, null, 2),
+      );
+
+      const { stdout: output, exitCode } = await execa(
+        "node",
+        [
+          path.join(localDistDirectory, "index.js"),
+          "check-lockfile-version-discrepancy",
+        ],
+        {
+          cwd: tempDirectory,
+        },
+      );
+      expect(output).toContain(
+        "package.json and package-lock.json versions in sync.",
+      );
+      expect(exitCode).toBe(0);
+    });
+  });
+});
