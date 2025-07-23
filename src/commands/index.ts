337bab83ed8ee4f2fa8090a7917e1a8b1eb85cbc
AlexMan123456 <alex.man03888@gmail.com>
2025-07-23 22:28:28 +0100

    Add an Execa helper to remove repetitive {stdio: "inherit"} code

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,12 @@
 import { Command } from "commander";
 import checkLockfileVersionDiscrepancy from "src/commands/check-lockfile-version-discrepancy";
-import gitCleanup from "src/commands/git-post-merge-cleanup";
+import gitPostMergeCleanup from "src/commands/git-post-merge-cleanup";
 import sayHello from "src/commands/say-hello";
 
 function loadCommands(program: Command) {
   sayHello(program);
   checkLockfileVersionDiscrepancy(program);
-  gitCleanup(program);
+  gitPostMergeCleanup(program);
 }
 
 export default loadCommands;
