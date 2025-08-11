739a6985ea9f85e5540ed21842d6b7c703559733
AlexMan123456 <alex.man03888@gmail.com>
2025-08-12 00:14:57 +0100

    Add ability to set the env file to write to

--- src/commands/edit-env.ts
+++ src/commands/edit-env.ts
@@ -10,23 +10,28 @@ function editEnv(program: Command) {
   program
     .command("edit-env <key> [value]")
     .description("Edit property in .env file (leave value blank to delete property)")
-    .action(async (key: string, value?: unknown) => {
+    .option("--file <file>", "The file to edit", ".env")
+    .action(async (key: string, value: unknown, { file }: { file: string }) => {
+      let newValue: unknown = value;
+      if (typeof newValue === "string" && newValue.startsWith("--")) {
+        newValue = undefined;
+      }
       let currentEnvFileContents: Record<string, unknown>;
       try {
         currentEnvFileContents = dotenv.parse(
-          await readFile(path.join(process.cwd(), ".env"), "utf-8"),
+          await readFile(path.join(process.cwd(), file), "utf-8"),
         );
       } catch {
         currentEnvFileContents = {};
       }
 
-      if (value !== undefined) {
-        currentEnvFileContents[key] = value;
+      if (newValue !== undefined) {
+        currentEnvFileContents[key] = newValue;
       } else {
         delete currentEnvFileContents[key];
       }
 
-      await writeFile(path.join(process.cwd(), ".env"), dotenvStringify(currentEnvFileContents));
+      await writeFile(path.join(process.cwd(), file), dotenvStringify(currentEnvFileContents));
       console.log(".env file updated");
     });
 }
--- tests/edit-env.test.ts
+++ tests/edit-env.test.ts
@@ -96,4 +96,63 @@ describe("edit-env", () => {
       expect(envFileContents.PROPERTY).toBe("test");
     });
   });
+  test("Writes to the specified file if --file flag given", async () => {
+    await temporaryDirectoryTask(async (temporaryPath) => {
+      const alexCLineTestClient = createAlexCLineTestClient({ cwd: temporaryPath });
+      await writeFile(
+        path.join(temporaryPath, ".env.test"),
+        dotenvStringify({
+          DATABASE_URL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          ENV: "test",
+        }),
+      );
+      await writeFile(
+        path.join(temporaryPath, ".env"),
+        dotenvStringify({
+          DATABASE_URL: "do-not-use-or-you-will-be.fired",
+          ENV: "production",
+        }),
+      );
+
+      await alexCLineTestClient("edit-env", [
+        "DATABASE_URL",
+        "https://youtu.be/jKUgVR29PJs?si=XUHJdBKR3pwWCncz",
+        "--file",
+        ".env.test",
+      ]);
+      const testEnvFileContents = dotenv.parse(
+        await readFile(path.join(temporaryPath, ".env.test"), "utf-8"),
+      );
+      const envFileContents = dotenv.parse(
+        await readFile(path.join(temporaryPath, ".env"), "utf-8"),
+      );
+      expect(testEnvFileContents.DATABASE_URL).toBe(
+        "https://youtu.be/jKUgVR29PJs?si=XUHJdBKR3pwWCncz",
+      );
+      expect(testEnvFileContents.ENV).toBe("test");
+
+      expect(envFileContents.DATABASE_URL).toBe("do-not-use-or-you-will-be.fired");
+      expect(envFileContents.ENV).toBe("production");
+    });
+  });
+  test("Deletes from custom file if no value specified but --file flag given", async () => {
+    await temporaryDirectoryTask(async (temporaryPath) => {
+      const alexCLineTestClient = createAlexCLineTestClient({ cwd: temporaryPath });
+      await writeFile(
+        path.join(temporaryPath, ".env.test"),
+        dotenvStringify({
+          DATABASE_URL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          ENV: "test",
+        }),
+      );
+
+      // De-rickroll command incoming!
+      await alexCLineTestClient("edit-env", ["DATABASE_URL", "--file", ".env.test"]);
+      const envFileContents = dotenv.parse(
+        await readFile(path.join(temporaryPath, ".env.test"), "utf-8"),
+      );
+      expect(envFileContents).not.toHaveProperty("DATABASE_URL");
+      expect(envFileContents.ENV).toBe("test");
+    });
+  });
 });
