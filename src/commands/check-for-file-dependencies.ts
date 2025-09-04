900ea2261fdb34e0b920d41b3ee3017474a36820
AlexMan123456 <alex.man03888@gmail.com>
2025-09-04 23:49:34 +0100

    Add script to check for existence of file dependencies
    This is useful when using the new use-local-components and use-live-components script, where we may sometimes forget to go back to using the live version of the package before committing. In cases like that, this script can be added to the linting script to ensure that local dependencies do not make it into production.

---
+++ src/commands/check-for-file-dependencies.ts
@@ -0,0 +1,64 @@
+import type { Command } from "commander";
+
+import { readFile } from "fs/promises";
+import path from "path";
+
+export interface PackageDependencies {
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+  peerDependencies?: Record<string, string>;
+}
+
+function findFileDependencies(
+  dependencies: Record<string, string> | undefined,
+): Record<string, string> {
+  const fileDependencies: Record<string, string> = {};
+  if (!dependencies) {
+    return {};
+  }
+  for (const dependency in dependencies) {
+    if (dependencies[dependency].includes("file:")) {
+      fileDependencies[dependency] = dependencies[dependency];
+    }
+  }
+  return fileDependencies;
+}
+
+function checkForFileDependencies(program: Command) {
+  program
+    .command("check-for-file-dependencies")
+    .description(
+      "Check for existence of file dependencies in package.json and give exit code 1 if such dependencies are found",
+    )
+    .action(async () => {
+      console.log("Checking for file dependencies...");
+      const { dependencies, devDependencies, peerDependencies } = JSON.parse(
+        await readFile(path.resolve(process.cwd(), "package.json"), "utf-8"),
+      );
+      const allFileDependencies: PackageDependencies = {
+        dependencies: findFileDependencies(dependencies),
+        devDependencies: findFileDependencies(devDependencies),
+        peerDependencies: findFileDependencies(peerDependencies),
+      };
+
+      if (Object.keys(allFileDependencies.dependencies ?? {}).length === 0) {
+        delete allFileDependencies.dependencies;
+      }
+      if (Object.keys(allFileDependencies.devDependencies ?? {}).length === 0) {
+        delete allFileDependencies.devDependencies;
+      }
+      if (Object.keys(allFileDependencies.peerDependencies ?? {}).length === 0) {
+        delete allFileDependencies.peerDependencies;
+      }
+
+      if (Object.keys(allFileDependencies).length !== 0) {
+        console.error("ERROR: File dependencies found:");
+        console.error(JSON.stringify(allFileDependencies, undefined, 2));
+        process.exit(1);
+      }
+      console.log("No file dependencies found!");
+      process.exit(0);
+    });
+}
+
+export default checkForFileDependencies;
