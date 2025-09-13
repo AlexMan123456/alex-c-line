ddc2cfeaa196acfe573848ca8d3fcb5c7b3cb046
AlexMan123456 <alex.man03888@gmail.com>
2025-09-14 00:52:31 +0100

    Add command to check for version number changes
    This is useful to have in a CI script so that we can enforce that changes to source code must also change the version number. This helps to ensure that the most recent published version of a package definitely matches the source code, reducing the change of any discrepancy between them that may make debugging harder.

---
+++ src/commands/check-version-number-change.ts
@@ -0,0 +1,61 @@
+import type { Command } from "commander";
+
+import { readFile } from "fs/promises";
+import path from "path";
+
+import { execa } from "execa";
+
+import { execaNoFail } from "src/utils/execa-helpers";
+
+function checkVersionNumberChange(program: Command) {
+  return program
+    .command("check-version-number-change")
+    .description(
+      "Check that version number on branch has changed if source code differs between main and current branch",
+    )
+    .action(async () => {
+      console.log("Checking for version change...");
+      const { stdout: currentBranch } = await execa`git branch --show-current`;
+      if (currentBranch === "main") {
+        console.error("❌ ERROR: Cannot check differences on main branch!");
+        process.exit(1);
+      }
+
+      const { exitCode } = await execaNoFail("git", ["diff", "origin/main...HEAD", "--quiet"]);
+      if (exitCode === 0) {
+        console.log("No source code changes found. Version does not need changing.");
+        process.exit(0);
+      }
+
+      const { stdout: packageContents } = await execa`git show origin/main:package.json`;
+      const { version: mainPackageVersion } = JSON.parse(packageContents);
+      const { version: currentBranchPackageVersion } = JSON.parse(
+        await readFile(path.resolve(process.cwd(), "package.json"), "utf-8"),
+      );
+
+      const [currentBranchMajor, currentBranchMinor, currentBranchPatch] =
+        currentBranchPackageVersion.split(".");
+
+      const newMajorVersion = `${parseInt(currentBranchMajor) + 1}.0.0`;
+      const newMinorVersion = `${currentBranchMajor}.${parseInt(currentBranchMinor) + 1}.0`;
+      const newPatchVersion = `${currentBranchMajor}.${currentBranchMinor}.${parseInt(currentBranchPatch) + 1}`;
+
+      if (mainPackageVersion === currentBranchPackageVersion) {
+        console.error("❌ Version needs updating. Please run one of the following:");
+        console.error(
+          `- npm version major -m "Change version number to v%s" (v${mainPackageVersion} -> v${newMajorVersion})`,
+        );
+        console.error(
+          `- npm version minor -m "Change version number to v%s" (v${mainPackageVersion} -> v${newMinorVersion})`,
+        );
+        console.error(
+          `- npm version patch -m "Change version number to v%s" (v${mainPackageVersion} -> v${newPatchVersion})`,
+        );
+        process.exit(1);
+      }
+
+      console.log("Version has been updated!");
+    });
+}
+
+export default checkVersionNumberChange;
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,7 @@ import type { Command } from "commander";
 
 import checkForFileDependencies from "src/commands/check-for-file-dependencies";
 import checkLockfileVersionDiscrepancy from "src/commands/check-lockfile-version-discrepancy";
+import checkVersionNumberChange from "src/commands/check-version-number-change";
 import editEnv from "src/commands/edit-env";
 import gitPostMergeCleanup from "src/commands/git-post-merge-cleanup";
 import sayHello from "src/commands/say-hello";
@@ -9,6 +10,7 @@ import sayHello from "src/commands/say-hello";
 function loadCommands(program: Command) {
   checkLockfileVersionDiscrepancy(program);
   checkForFileDependencies(program);
+  checkVersionNumberChange(program);
   editEnv(program);
   gitPostMergeCleanup(program);
   sayHello(program);
