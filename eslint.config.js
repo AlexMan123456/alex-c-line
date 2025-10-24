90618db5652522b6d8948731e352d6d9be88c122
AlexMan123456 <alex.man03888@gmail.com>
2025-10-25 00:18:02 +0100

    Use v2 of my ESLint plugin
    @alextheman/eslint-plugin has recently gone through a major update to change the configs structure. Now instead of plugin.configs.alexTypeScriptBase, we now access the base TypeScript config through plugin.configs['combined/typescript']

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import plugin from "@alextheman/eslint-plugin";
 
 export default [
-  ...plugin.configs.alexTypeScriptBase,
+  ...plugin.configs["combined/typescript"],
   {
     rules: {
       // Probably not the best idea to give warnings/errors when using the console in a console-based tool now, isn't it?
