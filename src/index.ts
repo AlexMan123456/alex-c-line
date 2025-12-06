171e876efcc5492c95cd9f5f3729b0b81e4868ee
AlexMan123456 <alex.man03888@gmail.com>
2025-12-06 14:17:37 +0000

    Use the actual version number

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import { version } from "package.json" with { type: "json" };
 
 import createCommands from "src/commands";
 
 const program = new Command();
-program.name("alex-c-line").description("CLI tool built by Alex").version("1.0.0");
+program.name("alex-c-line").description("CLI tool built by Alex").version(version);
 
 createCommands(program);
 program.parse(process.argv);
