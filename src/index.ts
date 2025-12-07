6267f3ea89015a3b377e788b9f752dfd9d9083bd
AlexMan123456 <alex.man03888@gmail.com>
2025-12-07 16:28:18 +0000

    Add update notifications
    This ensures that people who have the package locally installed can easily be informed of updates and don't miss out on new changes etc.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { version } from "package.json" with { type: "json" };
+import packageInfo from "package.json" with { type: "json" };
+import updateNotifier from "update-notifier";
 
 import createCommands from "src/commands";
 
 const program = new Command();
-program.name("alex-c-line").description("CLI tool built by Alex").version(version);
+program.name("alex-c-line").description("CLI tool built by Alex").version(packageInfo.version);
+
+updateNotifier({ pkg: packageInfo }).notify();
 
 createCommands(program);
 program.parse(process.argv);
