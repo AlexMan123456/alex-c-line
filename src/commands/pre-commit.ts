e47aeeaacd3b5a9f82056f796a4357ca56821048
AlexMan123456 <alex.man03888@gmail.com>
2026-01-02 10:43:56 +0000

    Centralise the pre-commits as an alex-c-line command
    This reduces the need to repeat the pre-commit code in all repositories. It's also especially nice now because it means the pre-commits can be done with TypeScript rather than Bash, which is good because Bash feels very clunky at times and using it gives me flashbacks to my GitHub Actions pain at times...
    
    Also note that I don't test this command. This is because it would be WAY too long and also a bit too meta, even for me. We'd be testing the thing that runs the tests to begin with, and that would be trippy, even if we set up a temporary directory like we do in other tests. If anything, it'd be worse with a temporary directory.

---
+++ src/commands/pre-commit.ts
@@ -0,0 +1,54 @@
+import type { Command } from "commander";
+
+import { execaNoFail } from "src/utility/execa-helpers";
+
+interface PreCommitOptions {
+  tests?: boolean;
+}
+
+function preCommit(program: Command) {
+  program
+    .command("pre-commit")
+    .description("Run the standard pre-commits used across all my repositories.")
+    .option("--no-tests", "Skip the tests")
+    .action(async ({ tests: shouldIncludeTests }: PreCommitOptions) => {
+      const { exitCode: diffExitCode } = await execaNoFail("git", ["diff", "--cached", "--quiet"]);
+
+      switch (diffExitCode) {
+        case 128:
+          program.error("Not currently in a Git repository", {
+            exitCode: 1,
+            code: "GIT_DIFF_FAILED",
+          });
+        // program.error() will throw an error and stop the program, so it is redundant to include a break here.
+        // eslint-disable-next-line no-fallthrough
+        case 0:
+          console.info("No staged changes found.");
+          return;
+      }
+
+      async function runCommandAndLogToConsole(command: string, args?: string[] | undefined) {
+        const result = await execaNoFail(command, args, { stdio: "inherit" });
+
+        if (result.exitCode !== 0) {
+          program.error(`Command failed: ${command}${args?.length ? ` ${args.join(" ")}` : ""}`, {
+            exitCode: result.exitCode ?? 1,
+            code: "PRE_COMMIT_FAILED",
+          });
+        }
+
+        return result;
+      }
+
+      await runCommandAndLogToConsole("pnpm", ["run", "build"]);
+      await runCommandAndLogToConsole("pnpm", ["run", "format"]);
+      await runCommandAndLogToConsole("pnpm", ["run", "lint"]);
+      if (shouldIncludeTests) {
+        await runCommandAndLogToConsole("pnpm", ["test"]);
+      }
+
+      await execaNoFail("git", ["update-index", "--again"]);
+    });
+}
+
+export default preCommit;
